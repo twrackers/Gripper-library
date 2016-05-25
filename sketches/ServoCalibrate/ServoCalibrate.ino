72e55ee10eea0608554ffa123168f798aa541a6d
Thomas W Rackers <twrackers@users.noreply.github.com>
2016-05-25 12:40:37 -0400

    Add files via upload

---
+++ sketches/ServoCalibrate/ServoCalibrate.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+#include <Gripper.h>
+
+int closed = 560;
+int opened = 1420;
+
+Gripper gripper(9, closed, opened, 220);
+
+void setup() 
+{
+  Serial.begin(115200);
+}
+
+void loop() 
+{
+  if (digitalRead(2) == 0) {
+    gripper.open();
+  } else if (digitalRead(3) == 0) {
+    gripper.close();
+  }
+  if (gripper.update()) {
+    Serial.println(gripper.isOpened());
+  }
+}
