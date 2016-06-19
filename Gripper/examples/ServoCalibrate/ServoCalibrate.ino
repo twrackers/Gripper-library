8b99d2b47f006a45a04b2901b452feaeade3af75
Thomas W Rackers <twrackers@gmail.com>
2016-06-19 01:11:26 -0400

    Updated to Arduino 1.5.0+ library format.

---
+++ Gripper/examples/ServoCalibrate/ServoCalibrate.ino
@@ -0,0 +1,21 @@
+#include <Gripper.h>
+
+int closed = 560;   // microseconds
+int opened = 1420;  // microseconds
+int timeout = 600;  // milliseconds
+
+Gripper gripper(9, closed, opened, timeout);
+
+void setup() 
+{
+}
+
+void loop() 
+{
+  if (digitalRead(2) == 0) {
+    gripper.open();
+  } else if (digitalRead(3) == 0) {
+    gripper.close();
+  }
+  gripper.update();
+}
